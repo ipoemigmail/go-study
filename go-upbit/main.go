186ae92a1d2bfcf4ecb6e352a4aca43e59713b07
ben.jung <ben.jung@kakaocommerce.com>
2021-05-21 16:53:49 +0900

    go-upbit change

--- go-upbit/main.go
+++ go-upbit/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/big"
 	"sort"
 	"strings"
 	"sync"
@@ -23,12 +24,19 @@ func CheckMarket(ms []upbit.MarketTicker) {
 	if len(ms) >= BUFFER_SIZE {
 		last := ms[len(ms)-1]
 		sort.Slice(ms, func(i, j int) bool {
-			return ms[i].TradePrice < ms[j].TradePrice
+			p1 := big.Float(ms[i].TradePrice)
+			p2 := big.Float(ms[j].TradePrice)
+			return p1.Cmp(&p2) < 0
 		})
 		min := ms[0]
-		diff := last.TradePrice - min.TradePrice
-		if last.TradePrice*0.1 < diff {
-			fmt.Printf("%s, last: %f, min: %f\n", last.Market, last.TradePrice, min.TradePrice)
+		lastPrice := big.Float(last.TradePrice)
+		minPrice := big.Float(min.TradePrice)
+		diff := big.NewFloat(0)
+		thresholdPrice := big.NewFloat(0)
+		thresholdPrice.Mul(&lastPrice, big.NewFloat(0.1))
+		diff.Sub(&lastPrice, &minPrice)
+		if thresholdPrice.Cmp(diff) < 0 {
+			fmt.Printf("%s, last: %v, min: %v\n", last.Market, lastPrice, minPrice)
 		}
 	}
 }
--- go-upbit/upbit/upbit.go
+++ go-upbit/upbit/upbit.go
@@ -3,6 +3,7 @@ package upbit
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"net/http"
 	"strings"
 
@@ -11,6 +12,19 @@ import (
 
 // BaseURL is
 const BaseURL = "https://api.upbit.com/v1"
+
+type BigDecimal big.Float
+
+func (x *BigDecimal) MarshalJSON() ([]byte, error) {
+	x1 := big.Float(*x)
+	result, err := x1.MarshalText()
+	return result, err
+}
+
+func (x *BigDecimal) UnmarshalJSON(data []byte) error {
+	x1 := big.Float(*x)
+	return x1.UnmarshalText(data)
+}
 
 // Market is
 type Market struct {
@@ -22,32 +36,32 @@ type Market struct {
 
 // MarketTicker is
 type MarketTicker struct {
-	Market             string  `json:"market"`
-	TradeDate          string  `json:"trade_date"`
-	TradeTime          string  `json:"trade_time"`
-	TradeDateKst       string  `json:"trade_date_kst"`
-	TradeTimeKst       string  `json:"trade_time_kst"`
-	TradeTimestamp     int64   `json:"trade_timestamp"`
-	OpeningPrice       float64 `json:"opening_price"`
-	HighPrice          float64 `json:"high_price"`
-	LowPrice           float64 `json:"low_price"`
-	TradePrice         float64 `json:"trade_price"`
-	PrevClosingPrice   float64 `json:"prev_closing_price"`
-	Change             string  `json:"change"`
-	ChangePrice        float64 `json:"change_price"`
-	ChangeRate         float64 `json:"change_rate"`
-	SignedChangePrice  float64 `json:"signed_change_price"`
-	SignedChangeRate   float64 `json:"signed_change_rate"`
-	TradeVolume        float64 `json:"trade_volume"`
-	AccTradePrice      float64 `json:"acc_trade_price"`
-	AccTradePrice24H   float64 `json:"acc_trade_price_24h"`
-	AccTradeVolume     float64 `json:"acc_trade_volume"`
-	AccTradeVolume24H  float64 `json:"acc_trade_volume_24h"`
-	Highest52WeekPrice float64 `json:"highest_52_week_price"`
-	Highest52WeekDate  string  `json:"highest_52_week_date"`
-	Lowest52WeekPrice  float64 `json:"lowest_52_week_price"`
-	Lowest52WeekDate   string  `json:"lowest_52_week_date"`
-	Timestamp          int64   `json:"timestamp"`
+	Market             string     `json:"market"`
+	TradeDate          string     `json:"trade_date"`
+	TradeTime          string     `json:"trade_time"`
+	TradeDateKst       string     `json:"trade_date_kst"`
+	TradeTimeKst       string     `json:"trade_time_kst"`
+	TradeTimestamp     int64      `json:"trade_timestamp"`
+	OpeningPrice       BigDecimal `json:"opening_price"`
+	HighPrice          BigDecimal `json:"high_price"`
+	LowPrice           BigDecimal `json:"low_price"`
+	TradePrice         BigDecimal `json:"trade_price"`
+	PrevClosingPrice   BigDecimal `json:"prev_closing_price"`
+	Change             string     `json:"change"`
+	ChangePrice        BigDecimal `json:"change_price"`
+	ChangeRate         float64    `json:"change_rate"`
+	SignedChangePrice  BigDecimal `json:"signed_change_price"`
+	SignedChangeRate   float64    `json:"signed_change_rate"`
+	TradeVolume        BigDecimal `json:"trade_volume"`
+	AccTradePrice      BigDecimal `json:"acc_trade_price"`
+	AccTradePrice24H   BigDecimal `json:"acc_trade_price_24h"`
+	AccTradeVolume     BigDecimal `json:"acc_trade_volume"`
+	AccTradeVolume24H  BigDecimal `json:"acc_trade_volume_24h"`
+	Highest52WeekPrice BigDecimal `json:"highest_52_week_price"`
+	Highest52WeekDate  string     `json:"highest_52_week_date"`
+	Lowest52WeekPrice  BigDecimal `json:"lowest_52_week_price"`
+	Lowest52WeekDate   string     `json:"lowest_52_week_date"`
+	Timestamp          int64      `json:"timestamp"`
 }
 
 // InternalError is
